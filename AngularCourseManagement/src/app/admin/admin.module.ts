34db45e672f0b997966e73e2581eff42cc1f294a
schute <schute@mobiquityinc.com>
2020-02-15 09:27:35 +0530

    [15-Feb-2020] : Created Login, Sign Up compnents

--- AngularCourseManagement/src/app/admin/admin.module.ts
+++ AngularCourseManagement/src/app/admin/admin.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, SignupComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
 })
 export class AdminModule { }
--- AngularCourseManagement/src/app/app-routing.module.ts
+++ AngularCourseManagement/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CourseListComponent } from './course/course-list/course-list.component';
 import { LoginComponent } from './admin/login/login.component';
+import { SignupComponent } from './admin/signup/signup.component';
 
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
   {path: 'home', component: HomeComponent},
   {path: 'courses', component: CourseListComponent},
   {path: '', redirectTo: 'login', pathMatch: 'full'}
